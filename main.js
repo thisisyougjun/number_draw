eb657619183f5b2f37cac51a35354aabf33e63dc
thisisyoungjun <122899@naver.com>
2022-02-22 23:56:11 +0900

    22-02-22

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-const rand1 = Math.floor(Math.random() *10);
+const target1 =document.getElementById("randForm");
 
-const target = document.getElementById("#randForm")
+const winnner =document.getElementsByClassName('winner');
+winnner.innerText='213123';
 
-document.getElementById("#randForm").addEventListener("submit", ButtonClick);
+function readExcel() {
+    let input = event.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        let data = reader.result;
+        let workBook = XLSX.read(data, { type: 'binary' });
+        workBook.SheetNames.forEach(function (sheetName) {
+            console.log('SheetName: ' + sheetName);
+            let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
+            target1.addEventListener("submit",Draw);
+            function Draw() {
+                event.preventDefault();
+                //console.log(JSON.stringify(rows));
+                const WinNum = Math.floor(rows.length * Math.random())
+                const Winnum_Object =rows[WinNum]
+                console.log(Winnum_Object);
+                const winnner_num =document.querySelector('.winner_num');
+                const winner_name =document.querySelector('.winner_name');
+                /*for (let i = 0; i <1; i++) {
+                    const key =Object.keys(Winnum_Object)[i]
+                    const value =Object.values(Winnum_Object)[i]
+                    winnner_num.innerHTML=key+' : '+value
 
-function ButtonClick(event){
-    event.preventDefault();
+                }
+                 */
+                const key =Object.keys(Winnum_Object)[0]
+                const value =Object.values(Winnum_Object)[0]
+                winnner_num.innerHTML=key+' : '+value
+                const key2 =Object.keys(Winnum_Object)[1]
+                const value2 =Object.values(Winnum_Object)[1]
+                winner_name.innerHTML=key2+' : '+value2
+                //const tessst = rows[1]
+
+
+            }
+        })
+    };
+    reader.readAsBinaryString(input.files[0]);
 }
 
-function rand(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
---
+++ test.js
@@ -0,0 +1,3 @@
+const test1 =[1,2,3,4,5,6,7,8,9]
+
+console.log(test1.splice(6));
