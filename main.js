824b8bda8b5a3c5840e9f711bfa34a0c469d85e6
thisisyoungjun <122899@naver.com>
2022-02-24 22:41:01 +0900

    22-02-24

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const target1 =document.getElementById("randForm");
+const target1 =document.querySelector(".DrawBtn");
 
 const winnner =document.getElementsByClassName('winner');
 winnner.innerText='213123';
@@ -12,34 +12,56 @@ function readExcel() {
         workBook.SheetNames.forEach(function (sheetName) {
             console.log('SheetName: ' + sheetName);
             let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
-            target1.addEventListener("submit",Draw);
+            target1.addEventListener("click",Draw);
             function Draw() {
                 event.preventDefault();
                 //console.log(JSON.stringify(rows));
-                const WinNum = Math.floor(rows.length * Math.random())
-                const Winnum_Object =rows[WinNum]
-                console.log(Winnum_Object);
-                const winnner_num =document.querySelector('.winner_num');
-                const winner_name =document.querySelector('.winner_name');
-                /*for (let i = 0; i <1; i++) {
-                    const key =Object.keys(Winnum_Object)[i]
-                    const value =Object.values(Winnum_Object)[i]
-                    winnner_num.innerHTML=key+' : '+value
 
-                }
-                 */
-                const key =Object.keys(Winnum_Object)[0]
-                const value =Object.values(Winnum_Object)[0]
-                winnner_num.innerHTML=key+' : '+value
-                const key2 =Object.keys(Winnum_Object)[1]
-                const value2 =Object.values(Winnum_Object)[1]
-                winner_name.innerHTML=key2+' : '+value2
-                //const tessst = rows[1]
+                const numbox = document.querySelector('.numBox')
+                let i =1;
+                const timer = setInterval(function (){
+                    if(i>30){
+                        clearInterval(timer)
+                    }else {
+                        const WinNum = Math.floor(rows.length * Math.random())
+                        const Winnum_Object =rows[WinNum]
+                        const key =Object.keys(Winnum_Object)
+                        const value =Object.values(Winnum_Object)
 
+                        const winnner_num =document.querySelector('.winner_num');
+                        const winner_name =document.querySelector('.winner_name');
+                        const winner_insta =document.querySelector('.winner_insta');
+                        const winner_size =document.querySelector('.winner_size');
+
+                        numbox.innerHTML=value[0]
+                        console.log(value[0]);
+                        i++
+
+                        for (const i in key) {
+                            if (i == 0) {
+                                winnner_num.innerHTML = key[i] + ' : ' + value[i]
+                            } else if (i == 1) {
+                                winner_name.innerHTML = key[i] + ' : ' + value[i]
+                            }
+                            else if (i == 2) {
+                                winner_insta.innerHTML = key[i] + ' : ' + value[i]
+                            }
+                            else if (i == 3) {
+                                winner_size.innerHTML = key[i] + ' : ' + value[i]
+                            }
+                        }
+
+                        winner_insta.addEventListener('click',instaLink)
+                        function instaLink(){
+                            location.href='https://www.instagram.com/'+value[2]+'/';
+                        }
+                    }
+                },40)
 
             }
         })
     };
     reader.readAsBinaryString(input.files[0]);
 }
+
 
