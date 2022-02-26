76a4f0eaa5654eab3ca4263cb5cf839291990bd3
thisisyoungjun <122899@naver.com>
2022-02-26 23:06:34 +0900

    22-02-26

--- main.js
+++ main.js
@@ -3,24 +3,33 @@ const target1 =document.querySelector(".DrawBtn");
 const winnner =document.getElementsByClassName('winner');
 winnner.innerText='213123';
 
+
+
 function readExcel() {
     let input = event.target;
     let reader = new FileReader();
+
+
+
     reader.onload = function () {
         let data = reader.result;
         let workBook = XLSX.read(data, { type: 'binary' });
+
         workBook.SheetNames.forEach(function (sheetName) {
             console.log('SheetName: ' + sheetName);
             let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
             target1.addEventListener("click",Draw);
+
+
+            console.log(origin)
+
             function Draw() {
                 event.preventDefault();
                 //console.log(JSON.stringify(rows));
-
                 const numbox = document.querySelector('.numBox')
-                let i =1;
+                let i =0;
                 const timer = setInterval(function (){
-                    if(i>30){
+                    if(i>=30){
                         clearInterval(timer)
                     }else {
                         const WinNum = Math.floor(rows.length * Math.random())
@@ -28,13 +37,14 @@ function readExcel() {
                         const key =Object.keys(Winnum_Object)
                         const value =Object.values(Winnum_Object)
 
-                        const winnner_num =document.querySelector('.winner_num');
-                        const winner_name =document.querySelector('.winner_name');
-                        const winner_insta =document.querySelector('.winner_insta');
-                        const winner_size =document.querySelector('.winner_size');
 
-                        numbox.innerHTML=value[0]
-                        console.log(value[0]);
+                        const winnner_num =document.querySelector('#winner_num');
+                        const winner_name =document.querySelector('#winner_name');
+                        const winner_insta =document.querySelector('#winner_insta');
+                        const winner_size =document.querySelector('#winner_size');
+
+                        numbox.innerHTML=value[2]
+                        console.log(value[2])
                         i++
 
                         for (const i in key) {
@@ -51,17 +61,35 @@ function readExcel() {
                             }
                         }
 
-                        winner_insta.addEventListener('click',instaLink)
+                        const Targetnumbox = document.querySelector(".numBox")
+                        Targetnumbox.addEventListener('click',instaLink)
                         function instaLink(){
-                            location.href='https://www.instagram.com/'+value[2]+'/';
+                           window.open(`https://www.instagram.com/${Targetnumbox.innerHTML}/`,"_blank")
+
+                            console.log(`${value[2]}테스트 `)
                         }
+                        
+                        function cursorPointerOn(){
+                            Targetnumbox.style.cursor='pointer'
+                        }
+                        cursorPointerOn();
+
+                        let numboxFontsize = Targetnumbox.style.fontSize
+                        if (Targetnumbox.innerHTML.length>10){
+                            document.querySelector(".numBox").style.fontSize='50px'
+                        }else if(Targetnumbox.innerHTML.length<10){
+                            document.querySelector(".numBox").style.fontSize='60px'
+                        }
+
+
                     }
+
                 },40)
 
             }
         })
     };
     reader.readAsBinaryString(input.files[0]);
-}
 
+}
 
--- test.js
+++
@@ -1,3 +0,0 @@
-const test1 =[1,2,3,4,5,6,7,8,9]
-
-console.log(test1.splice(6));
